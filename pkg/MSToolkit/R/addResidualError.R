323ecab52e17a42cda3b328cead8d0377f1aad5a
mks21471 (Mike K Smith) <michael.smith12@tesco.net>
2009-09-29 16:48:45 +0000

    MKS - 29/9/09 Rolled back to version 2.0 function. Will use v2.0 code for CRAN version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mstoolkit@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/MSToolkit/R/addResidualError.R
+++ pkg/MSToolkit/R/addResidualError.R
@@ -2,8 +2,6 @@ addResidualError <- function(
   response,                       #@ numeric vector of response data
   covariance,                     #@ lower triangle or matrix
   errStruc = "additive",          #@ function describing how to apply residual error
-#@ For multivariate normal errors i.e. full variance-covariance on residual error
-#@ prefer errStruc = "None" by default (like createNormalParameters)
   seed = .deriveFromMasterSeed( ) #@ Random Seed to use
   ) { 
   ################################################################################
@@ -11,11 +9,6 @@ addResidualError <- function(
   # addResidualError.R Tue Jun 19 16:17:20 BST 2007 @678 /Internet Time/
   #
   # Author: Romain
-  ################################################################################
-  # Mike K Smith, Sandwich CT13 9NJ 2009
-  # Revised to include multivariate residual error structure
-  #
-  # Mon 28th Sept 2009 21:48 BST
   ################################################################################
   # DESCRIPTION: add residual error to a response
   # KEYWORDS: component:response
@@ -25,43 +18,32 @@ addResidualError <- function(
   .requiredArgs(covariance, "The `covariance` is required")
   set.seed(seed)
 
-  # For multivariate residual error structure
-   npar <- length(diag(covariance))
-   
-  covariance <- parseCovMatrix( covariance, npar)                  
+  # <TODO>
+  # currently handle only one variable
+  covariance <- parseCovMatrix( covariance, 1)                  
+  # </TODO>
 
   errFun <- if(is.function(errStruc)) errStruc else {
-    errStruc <- initialChar( errStruc, "nap", 
-      "`errStruc` should be `none`,`additive`, `proportional` or a function")
+    errStruc <- initialChar( errStruc, "ap", 
+      "`errStruc` should be `additive`, `proportionnal` or a function")
     switch( errStruc, 
-	   "n" = function(x,y) y,        # none - residual error is an array
        "a" = function(x,y) x+y,      # additive
        "p" = function(x,y) exp(x+y)  # proportional
        )
   }
   
-  if( npar > 1){
-    error <- mvrnorm( n=length(response), mu=rep(0,npar),
-	Sigma = covariance)
-	}
-  
-  if( npar==1){
   error <- rnorm( length(response), mean = 0, 
     sd = sqrt(covariance[1,1]) )
-  }
   
   if( length(formals(errFun)) <2  ){
     ectdStop("The error function should take at least two arguments")
   }
   
   out <- errFun( response, error )
-  if( npar==1 & out %!of% "numeric"){
+  if( out %!of% "numeric"){
     ectdStop("The error function should return a numeric vector") 
   }
-  if( npar>1 & out %!of% "matrix"){
-    ectdStop("The error function should return an array for >1 covariance value") 
-  }
-  if( npar==1 & out %!l% response ){     
+  if( out %!l% response ){     
     ectdStop(
       "The error function supplied generates a vector that does not have" %.nt%
       "the same length as the response vector supplied" 
@@ -71,3 +53,4 @@ addResidualError <- function(
   
   
 }
+
